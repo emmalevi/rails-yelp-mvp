88002edee00f4f7423405c8d5fc658249d1c1f5b
emma levi <emmaa-l@hotmail.fr>
2020-03-27 13:31:15 +0300

    Rails yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get "restaurants", to: "restaurants#index"
+get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+post "restaurants", to: "restaurants#create"
+get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+# NB: The `show` route needs to be *after* `new` route.
+get "restaurants/:restaurant_id/reviews",     to: "reviews#index"
+get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: :new_review
+post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: :restaurant_reviews
+get "reviews/:id", to: "reviews#show"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:      'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:      'italian'
+  },
+   {
+    name:         'Pizeria',
+    address:      '56 rue de la Sablière',
+    category:     'italian'
+  },
+  {
+    name:         'Bristol',
+    address:      '46 rue Brun',
+    category:     'japanese'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+
+puts 'Finished!'
